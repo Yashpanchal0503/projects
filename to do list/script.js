bbff7a62057ddccdb05a9382268c8c2d488d27bf
YashPanchal0503 <yash2007panchal@gmail.com>
2025-06-23 11:08:29 +0530

    now tasks are save  after refresh also

--- to do list/script.js
+++ to do list/script.js
@@ -14,14 +14,23 @@ function addtask() {
         el.appendChild(span);
     }
     content.value='';
+    saveData()
 }
 contanier.addEventListener("click", function(e) {
     if (e.target.tagName === "LI") {
         e.target.classList.toggle("checked"); 
+        saveData()
     } else if (e.target.tagName === "SPAN") {
         e.target.parentElement.remove();
+        saveData()
     }
 }, false);
 
+function saveData(){
+    localStorage.setItem("data", listContainer.innerHTML);
+}
 
-
+function showTask(){
+    listContainer.innerHTML= localStorage.getItem("data");
+}
+showTask()
